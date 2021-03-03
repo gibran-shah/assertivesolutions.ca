3908d378eae02ac8c5c8d7b22b456487accca229
Gibran Shah <gshah.hab@outlook.com>
2021-03-02 21:27:20 -0700

    Made blog images into background on main page

--- src/OurBlog/BlogPost/BlogPost.js
+++ src/OurBlog/BlogPost/BlogPost.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import './BlogPost.scss';
-
 class BlogPost extends Component {
     render() {
+        const backgroundStyles = {
+          width: '100%',
+          height: '15vw',
+          marginBottom: '20px',
+          backgroundImage: `url(${this.props.blogImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundColor: 'red'
+        };
+
         return (
             <div className="blog-item flex-column-space-between">
                 <div className="blog-img-and-date">
-                    <img className="blog-img" src={this.props.blogImg} alt={this.props.title} />
+                    <div style={backgroundStyles}></div>
                     <div className="blog-date"><i className="far fa-calendar-alt">&nbsp;</i>{this.props.date}</div>
                 </div>  
                 <span className="blog-title">{this.props.title}</span>
                 <span className="blog-blurb">{this.props.blurb}</span>
-                {/* <span className="blog-blurb">blah blah blah</span> */}
                 <a className="read-more-link" href="#" onClick={(e) => {this.props.blogClicked(e, this.props.title)}}>read more</a>
             </div>
         );
--- src/OurBlog/OurBlog.js
+++ src/OurBlog/OurBlog.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import './OurBlog.scss';
 import BlogPost from './BlogPost/BlogPost';
-import BlogImg1 from '../assets/images/blog img 1.png'
-import BlogImg2 from '../assets/images/blog img 2.png'
-import BlogImg3 from '../assets/images/blog img 3.png'
+import axios from '../axios/axiosInstance';
+import moment from 'moment';
 
 class OurBlog extends Component {
 
-    blogClicked(e, blogTitle) {
+    constructor() {
+      super();
+
+      this.state = {
+        blogs: []
+      };
+    }
+
+    blogClicked(e, blogId) {
         e.preventDefault();
     }
 
     render() {
+        let blogs = [];
+        if (this.state.blogs.length) {
+          for (let i = 0; i < 3; i++) {
+            const blog = this.state.blogs[i];
+            blogs.push(
+              <BlogPost
+                key={blog.id}
+                blogImg={blog.imageUrl[0]}
+                date={moment.unix(blog.updatedAt/1000).format('MMM DD, YYYY')}
+                title={blog.title}
+                blurb={blog.body}
+                blogClicked={(e) => { this.blogClicked(e, blog.id) }}
+              />
+            );
+          }
+        }
+
         return (
             <section>
                 <a name="our-blog" />
@@ -20,29 +44,28 @@ class OurBlog extends Component {
                         <span className="heading-text our-blog-heading">our <span className="primary-text">blog</span></span>
                     </div>
                     <div className="our-blog-content">
-                        <BlogPost
-                            blogImg={BlogImg1}
-                            date="Aug 1 2019"
-                            title="blog 1"
-                            blurb="blah blah blah"
-                            blogClicked={this.blogClicked} />
-                        <BlogPost
-                            blogImg={BlogImg2}
-                            date="Aug 2 2019"
-                            title="blog 2"
-                            blurb="blah blah blah blah"
-                            blogClicked={this.blogClicked} />
-                        <BlogPost
-                            blogImg={BlogImg3}
-                            date="Aug 3 2019"
-                            title="blog 3"
-                            blurb="blah blah blah blah blah blah blah blah blah blah blah blah"
-                            blogClicked={this.blogClicked} />
+                      {blogs}
                     </div>
                 </div>
             </section>
         );
     }
+
+    componentDidMount() {    
+      axios.get('/blogs').then(response => {
+          if (response.data) {
+              const entries = Object.entries(response.data);
+              const allPosts = entries.map(p => Object.assign({id: p[0]}, {...p[1]}))
+                .sort((p1, p2) => p1.updatedAt > p2.updatedAt ? -1 : 1)
+                .slice(0, 3);
+              this.setState({
+                  blogs: allPosts
+              });
+          }
+      }, err => {
+          console.log('err=', err);
+      });
+  }
 }
 
 export default OurBlog;
