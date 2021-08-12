c9eabffc46e5041f0f0138da1f5df32291ab7751
Gibran Shah <gshah.hab@outlook.com>
2021-08-11 20:17:40 -0600

    linked facebook icon to facebook account

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -14,9 +14,15 @@ class Footer extends Component {
                         <img className="footer-logo-img footer-item" src={AssertiveSolutionsLogo} alt="logo.png" />
                         <img className="assertive-solutions footer-item" src={AssertiveSolutionsFooter} alt="assertive solutions footer.png" />
                         <div className="social-media flex-row-space-around footer-item">
+                          <a href="https://www.facebook.com/Assertive-Solutions-Inc-729853577771300">
                             <i className="fab fa-facebook-square"></i>
+                          </a>
+                          <a href="#">
                             <i className="fab fa-linkedin"></i>
+                          </a>
+                          <a href="#">
                             <i className="fab fa-instagram"></i>
+                          </a>
                         </div>
                     </div>
                 </div>
